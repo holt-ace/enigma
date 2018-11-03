93d34b0a97327bba554edef61b8ea6b07f884099
Asa Holt <holt.ace@icloud.com>
2018-11-03 11:46:11 -0600

    added random key test

--- test/key_test.rb
+++ test/key_test.rb
@@ -6,4 +6,10 @@ class KeyTest < Minitest::Test
     key = Key.new
     assert_instance_of Key, key
   end
+
+  def test_generate_random_key_with_padded_zeroes
+    key = Key.new
+    expected = [Integer, Integer, Integer, Integer, Integer]
+    assert_equal expected, key.random_key
+  end
 end
