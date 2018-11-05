0da5ab3bf2a33ee0cd1c3b972a967729b46da978
Asa Holt <holt.ace@icloud.com>
2018-11-05 16:23:18 -0700

    update coder test with correct tests

--- test/coder_test.rb
+++ test/coder_test.rb
@@ -1,6 +1,23 @@
 require './test/test_helper'
 require './lib/coder'
+require './lib/shift'
 
 class CoderTest < Minitest::Test
-  
+  def test_it_exists
+    shift = Shift.new("02715", "040895")
+    coder = Coder.new(shift)
+    assert_instance_of Coder, coder
+  end
+
+  def test_it_encodes
+    shift = Shift.new("02715", "040895")
+    coder = Coder.new(shift)
+    assert_equal "keder ohulw", coder.encode("hello world")
+  end
+
+  def test_it_decodes
+    shift = Shift.new("02715", "040895")
+    coder = Coder.new(shift)
+    assert_equal "hello world", coder.decode("keder ohulw")
+  end
 end
