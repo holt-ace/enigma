75570f95e493e648d2d4e3a7e603b52fe48ba7b6
Asa Holt <holt.ace@icloud.com>
2018-11-05 14:09:26 -0700

    implements integer returns in key class and corrects randomization

--- lib/key.rb
+++ lib/key.rb
@@ -3,25 +3,25 @@ class Key
   attr_reader :random_key
   def initialize(key_string = nil)
     if key_string == nil
-        @random_key = rand(5**6).to_s.rjust(5, '0')
+        @random_key = rand(0..99999).to_s.rjust(5, '0')
     else
       @random_key = key_string
     end
   end
 
   def rotation_a
-    "#{@random_key[0]}#{@random_key[1].to_i}"
+    "#{@random_key[0]}#{@random_key[1]}".to_i
   end
 
   def rotation_b
-    "#{@random_key[1]}#{@random_key[2].to_i}"
+    "#{@random_key[1]}#{@random_key[2]}".to_i
   end
 
   def rotation_c
-    "#{@random_key[2]}#{@random_key[3].to_i}"
+    "#{@random_key[2]}#{@random_key[3]}".to_i
   end
 
   def rotation_d
-    "#{@random_key[3]}#{@random_key[4].to_i}"
+    "#{@random_key[3]}#{@random_key[4]}".to_i
   end
 end
