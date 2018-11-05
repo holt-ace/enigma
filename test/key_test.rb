238b304d511aa29caf05a0e13d337874826a6d1f
Asa Holt <holt.ace@icloud.com>
2018-11-05 14:05:12 -0700

    refactor key test

--- test/key_test.rb
+++ test/key_test.rb
@@ -10,33 +10,9 @@ class KeyTest < Minitest::Test
 
   def test_generate_random_key_with_padded_zeroes
     key = Key.new
-    expected = /[0-9]{5}/.match(key.random_key)
-    assert expected
+    assert (0..99999).member?(key.random_key.to_i)
   end
 
-  def test_rotation_a
-    key = Key.new
-    expected = /[0-9]{2}/.match(key.rotation_a)
-    assert expected
-  end
-
-  def test_rotation_b
-    key = Key.new
-    expected = /[0-9]{2}/.match(key.rotation_b)
-    assert expected
-  end
-
-  def test_rotation_c
-    key = Key.new
-    expected = /[0-9]{2}/.match(key.rotation_c)
-    assert expected
-  end
-
-  def test_rotation_d
-    key = Key.new
-    expected = /[0-9]{2}/.match(key.rotation_d)
-    assert expected
-  end
 
   def test_given_argument_calculates_correctly
     key = Key.new("02715")
