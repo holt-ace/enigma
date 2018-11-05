c2d51439b2885c571686613efcb1798cd7664329
Asa Holt <holt.ace@icloud.com>
2018-11-05 13:22:58 -0700

    updated offset to implement given date funtionality

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,14 +1,18 @@
 #calculates offset based on today's date and stores the date in correct format
 class Offset
   attr_reader :format_date
-  def initialize
-    @format_date = Date.today.strftime("%m%d%y")
+  def initialize(date_string = nil)
+    if date_string == nil
+      @format_date = Date.today.strftime("%m%d%y")
+    else
+      @format_date = date_string
+    end
   end
 
   def last_four
     numeric = @format_date.to_i
-    square = numeric**numeric
-    square.to_s[6..10]
+    square = numeric**2
+    square.to_s[-4..-1]
   end
 
   def offset_a
