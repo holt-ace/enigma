a57b06509d1c36d3ff514a88c3449f1e3deec0d8
Asa Holt <holt.ace@icloud.com>
2018-11-05 13:21:43 -0700

    updated key test

--- test/key_test.rb
+++ test/key_test.rb
@@ -7,6 +7,7 @@ class KeyTest < Minitest::Test
     assert_instance_of Key, key
   end
 
+
   def test_generate_random_key_with_padded_zeroes
     key = Key.new
     expected = /[0-9]{5}/.match(key.random_key)
@@ -35,5 +36,14 @@ class KeyTest < Minitest::Test
     key = Key.new
     expected = /[0-9]{2}/.match(key.rotation_d)
     assert expected
+  end
+
+  def test_given_argument_calculates_correctly
+    key = Key.new("02715")
+
+    assert_equal "02", key.rotation_a
+    assert_equal "27", key.rotation_b
+    assert_equal "71", key.rotation_c
+    assert_equal "15", key.rotation_d
   end
 end
