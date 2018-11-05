36fdc30f96f430291e6aeb7d403b862f2fbf9dc0
Asa Holt <holt.ace@icloud.com>
2018-11-05 16:44:34 -0700

    implements encode method in coder class and turns Coder into module

--- lib/coder.rb
+++ lib/coder.rb
@@ -1,31 +1,28 @@
 #this does me encoding and decoding, it will inherit into Enigma
 require_relative 'shift'
-class Coder
-  def initialize(shift)
-    @shift = shift
-  end
+module Coder
 
-  def encode(message)
+  def encode(message, shift)
     chars = [*"a".."z"] << " "
     chunks = message.downcase.scan(/.{1,4}/)
     encrypted_chunks = chunks.map do |chunk|
       chunk.split("").map.with_index do |letter, i|
         index = chars.find_index(letter)
         if i == 0
-          chars.rotate(@shift.shift_a)[index]
+          chars.rotate(shift.shift_a)[index]
         elsif i == 1
-          chars.rotate(@shift.shift_b)[index]
+          chars.rotate(shift.shift_b)[index]
         elsif i == 2
-          chars.rotate(@shift.shift_c)[index]
+          chars.rotate(shift.shift_c)[index]
         elsif i == 3
-          chars.rotate(@shift.shift_d)[index]
+          chars.rotate(shift.shift_d)[index]
         end
       end
     end
     encrypted_chunks.flatten.join
   end
 
-  def decode(message)
+  def decode(message, shift)
 
   end
 
