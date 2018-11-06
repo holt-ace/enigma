09a0987c734305f5e5bc1efdc3a62abaf24b3daa
Asa Holt <holt.ace@icloud.com>
2018-11-05 17:16:11 -0700

    implement decode method for Coder (module)

--- lib/coder.rb
+++ lib/coder.rb
@@ -22,8 +22,24 @@ module Coder
     encrypted_chunks.flatten.join
   end
 
-  def decode(message, shift)
-
+  def decode(ciphertext, shift)
+    chars = [*"a".."z"] << " "
+    chunks = ciphertext.downcase.scan(/.{1,4}/)
+    decrypted_chunks = chunks.map do |chunk|
+      chunk.split("").map.with_index do |letter, i|
+        index = chars.find_index(letter)
+        if i == 0
+          chars.rotate(-shift.shift_a)[index]
+        elsif i == 1
+          chars.rotate(-shift.shift_b)[index]
+        elsif i == 2
+          chars.rotate(-shift.shift_c)[index]
+        elsif i == 3
+          chars.rotate(-shift.shift_d)[index]
+        end
+      end
+    end
+    decrypted_chunks.flatten.join
   end
 
 end
