10aeacd792c0d505aa7e7e02a432af6ad01250e2
Asa Holt <holt.ace@icloud.com>
2018-11-05 17:49:49 -0700

    update decrypt to recieve arguments from the terminal and output text

---
+++ lib/decrypt.rb
@@ -0,0 +1,12 @@
+require_relative 'enigma'
+enigma = Enigma.new
+key = ARGV[2]
+date = ARGV[3]
+new_file = File.new(ARGV[1], "w")
+
+File.foreach(ARGV[0]) do |line|
+  new_message = enigma.decrypt(line, key, date)
+  new_file.puts(new_message[:decryption])
+end
+puts "Created '#{ARGV[1]}' with the key #{key} and date #{date}"
+new_file.close
